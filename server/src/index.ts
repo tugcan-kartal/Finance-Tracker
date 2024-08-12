415ff09e1e74b3b37b9292095e8923700a5d90f5
tugcan-kartal <tugcankartal01@gmail.com>
2024-08-12 22:47:42 +0300

    first commit

---
+++ server/src/index.ts
@@ -0,0 +1,18 @@
+import express,{Express} from "express";
+import mongoose from "mongoose";
+import financialRecordRouter from "./routes/financial-records";
+
+const app: Express=express();
+const port=process.env.PORT || 3001;
+
+app.use(express.json());
+
+const mongoURI: string="mongodb+srv://tugcankartal01:217721Tt_@cluster0.kzmma.mongodb.net/";
+
+mongoose.connect(mongoURI).then(()=>console.log("Connected to MongoDB")).catch((err)=>console.error("Failed to Connect MongoDB",err));
+
+app.use("/financial-records",financialRecordRouter);
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}`);
+})
---
+++ server/src/routes/financial-records.ts
@@ -0,0 +1,6 @@
+import express,{Request,Response} from "express";
+import financialRecordModel from "../schema/financial-record";
+
+const router=express.Router();
+
+export default router;
